4d27eb120adb2355d06c5bcc3a3fd243b7aae178
xtxme <TT.icy013@gmail.com>
2024-09-04 18:05:06 +0700

    Change Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Tanapron Tangpadungsuk"
+            studentId="660610762"
           />
         </div>
       </body>
